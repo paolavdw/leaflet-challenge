1babda2c3cd04638c3c90fdef25895d090724df6
pvdw <paola.guigou@gmail.com>
2024-09-10 12:48:02 -0400

    uploading corrected js & css files

--- Leaflet-Part-1/Starter_Code (10)/Starter_Code/static/js/logic.js
+++ Leaflet-Part-1/Starter_Code (10)/Starter_Code/static/js/logic.js
@@ -1,11 +1,15 @@
+/// Create map object 
 var map = L.map('map').setView([38, -99], 4.5);
 
+/// Add title layer
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+/// Store API endpoint as queryURL
 let queryURL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
 
+/// Get request to queryURL to create, add and style Circle Markers
 d3.json(queryURL).then(data =>{
     L.geoJSON(data, {
         pointToLayer:function(geoJsonPoint, latlng) {
@@ -44,27 +48,26 @@ d3.json(queryURL).then(data =>{
         `;
     }).addTo(map);
 
-    // Create a legend control
+    /// Create a legend control
     let legend = L.control({ position: 'bottomright' });
-    // When the legend is added to the map
+    /// When the legend is added to the map
     legend.onAdd = function () {
       let div = L.DomUtil.create('div', 'info legend');
       let depth = ['-10-10', '10-30', '30-50', '50-70', '70-90', '90+'];
       let colors = ['green', 'lime', 'yellow', 'orange', 'darkorange', 'red'];
       let labels = [];
 
-      // Loop through the grades and generate a label with a colored square for each interval
+    /// Loop through the depth variable and generate a label with a colored square for each interval
     for (var i = 0; i < depth.length; i++) {
         labels.push('<i style="background:' + colors[i] + '; margin-right: 10px;"></i>' +
-            depth[i] +  (depth[i + 1] ? '' + '<br>' : '')); // removed + depth[i + 1]  & &ndash;
+            depth[i] +  (depth[i + 1] ? '' + '<br>' : '')); 
             ///console.log(labels[i])
     }
     div.innerHTML += '<h4>Earthquakes (Past Day) <br /> *Depth of Earthquakes* </h4>' + labels.join('');
     return div;
 
-
   };
-
+/// Add lengend to map
 legend.addTo(map);
 
 });
